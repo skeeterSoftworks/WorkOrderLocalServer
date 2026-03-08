26b808d0c16e0924c0edcc9989f66738de3c5f5a
skeeterSoftworks <ghostskeeter@gmail.com>
2026-03-08 20:10:39 +0100

    init

---
+++ src/main/java/com/skeeterSoftworks/WorkOrderLocal/WorkOrderLocalApplication.java
@@ -0,0 +1,13 @@
+package com.skeeterSoftworks.WorkOrderLocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkOrderLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrderLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skeeterSoftworks/WorkOrderLocal/WorkOrderLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skeeterSoftworks.WorkOrderLocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOrderLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
