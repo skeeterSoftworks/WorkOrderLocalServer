83f3547942a74da381faae8fcdecbe7b3ab6ef6f
skeeterSoftworks <ghostskeeter@gmail.com>
2026-03-08 22:37:59 +0100

    mockqr

---
+++ src/main/java/com/skeeterSoftworks/WorkOrderLocal/to/objects/MockQrRequest.java
@@ -0,0 +1,14 @@
+package com.skeeterSoftworks.WorkOrderLocal.to.objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MockQrRequest {
+
+	private String mockScannedQr;
+
+}
